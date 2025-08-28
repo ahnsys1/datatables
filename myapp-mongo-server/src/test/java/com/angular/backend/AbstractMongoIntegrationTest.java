341b0bfeca4458e959f9a22eeee6a12a8b5bc828
Your Name <you@example.com>
2025-08-28 23:57:37 +0200

    Refactor employee form validation and improve MongoDB integration test setup

--- myapp-mongo-server/src/test/java/com/angular/backend/AbstractMongoIntegrationTest.java
+++ myapp-mongo-server/src/test/java/com/angular/backend/AbstractMongoIntegrationTest.java
@@ -1,18 +1,22 @@
 package com.angular.backend;
 
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles("test")
-@Testcontainers
 public abstract class AbstractMongoIntegrationTest {
 
-    @ServiceConnection
-    @Container
-    public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.23-jammy")
-            .withReuse(true);
+    static final MongoDBContainer mongoDBContainer;
 
+    static {
+        mongoDBContainer = new MongoDBContainer("mongo:7.0");
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 }
