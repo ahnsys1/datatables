2d065c2d25311000186f42c9a3f5f3e1a86caa7b
Jan <jan@example.com>
2026-09-16 21:27:58 +0200

    feat: add Vault integration with Docker configuration and update network settings

--- run_rest_of_infrastructure.sh
+++ run_rest_of_infrastructure.sh
@@ -1,10 +1,9 @@
  #!/bin/bash
     
     echo "Starting Infrastructure..."
-    cd infrastructure/rest
+    cd infrastructure/rest-of-infrastructure
     read -rsp 'Vault token: ' VAULT_TOKEN
     export VAULT_TOKEN
-    cd infrastructure/rest
     echo "Starting Docker Compose with Vault token entered"
     sh docker-compose-rest.sh up --build
     unset VAULT_TOKEN
